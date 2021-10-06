36ecd50e54f48dc8c022c943c0b0c083c5d614c3
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-06 12:57:40 +0000

    adding file

--- components/frontend.sh
+++ components/frontend.sh
@@ -28,8 +28,8 @@ systemctl start nginx &>>/tmp/log && systemctl enable nginx &>>/tmp/log && syste
 status_check $?
 
 Filebeat
-curl "https://raw.githubusercontent.com/dasari97/PFL/master/components/own_filebeat.yml" >/tmp/filebeat
-bash /tmp/filebeat  >/etc/filebeat/filebeat.yml 
+#curl "https://raw.githubusercontent.com/dasari97/PFL/master/components/own_filebeat.yml" >/tmp/filebeat
+#bash /tmp/filebeat  >/etc/filebeat/filebeat.yml 
 systemctl restart filebeat
 echo -e "\e[1;32mFrontend component is ready to use.\e[0m"
 
