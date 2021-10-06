f4d6ca50ea67017466c2c07fef6aebe9e981daf4
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-06 13:13:41 +0000

    adding file

--- components/frontend.sh
+++ components/frontend.sh
@@ -30,7 +30,6 @@ status_check $?
 Filebeat
 curl -s "https://raw.githubusercontent.com/dasari97/PFL/master/components/own_filebeat.yml" >/tmp/filebeat
 bash /tmp/filebeat  >/etc/filebeat/filebeat.yml 
-rm /tmp/filebeat
 systemctl restart filebeat
 echo -e "\e[1;32mFrontend component is ready to use.\e[0m"
 
